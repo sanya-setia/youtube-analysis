76a8a7f2109194dbb722224811ee94caf3d4b9ba
Sanya Setia <sanyasetia07@gmail.com>
2025-04-06 18:48:18 +0100

    total video seconds for top 5

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import os
+from datetime import timedelta
 from dotenv import load_dotenv
-load_dotenv()
+from datetime import timedelta
 from googleapiclient.discovery import build
+import os
+import re
+load_dotenv()
 
 #Adding our API Key safely
 api_key = os.getenv('API_KEY')
@@ -9,11 +12,44 @@ api_key = os.getenv('API_KEY')
 #Building the service
 youtube = build('youtube', 'v3', developerKey=api_key)
 
-request = youtube.channels  ().list(
-    part='contentDetails',
-    forUsername='schafer5'
+pl_request = youtube.playlistItems().list(
+    part="contentDetails",
+    playlistId="PL-osiE80TeTvipOqomVEeZ1HRrcEvtZB_"
     )
 
-response = request.execute()
+pl_response = pl_request.execute()
+vid_ids = []
+for item in pl_response['items']:
+    vid_ids.append(item['contentDetails']['videoId'])
 
-print(response)
+print(','.join(vid_ids))
+
+vid_request = youtube.videos().list(
+    part="contentDetails",
+    id=','.join(vid_ids)
+)
+
+vid_response = vid_request.execute()
+
+hours_pattern = re.compile(r'(\d+)H')
+minutes_pattern= re.compile(r'(\d+)M')
+seconds_pattern = re.compile(r'(\d+)S')
+
+for item in vid_response['items']:
+    duration = item['contentDetails']['duration']
+    hours = hours_pattern.search(duration)
+    minutes = minutes_pattern.search(duration)
+    seconds = seconds_pattern.search(duration)
+    
+    hours = int(hours.group(1)) if hours else 0
+    minutes = int(minutes.group(1)) if minutes else 0
+    seconds = int(seconds.group(1)) if seconds else 0
+    
+    video_seconds = timedelta(
+        hours= hours,
+        minutes= minutes,
+        seconds= seconds
+    ).total_seconds()
+    
+    print(video_seconds)
+    print()
