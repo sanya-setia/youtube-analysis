8021b9fae75a774b4096fdad6603da3e367963ef
Sanya Setia <sanyasetia07@gmail.com>
2025-04-06 15:35:35 +0100

    Working Youtube API

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
+from googleapiclient.discovery import build
 
 #Adding our API Key safely
 api_key = os.getenv('API_KEY')
+
+#Building the service
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+request = youtube.channels  ().list(
+    part='contentDetails',
+    forUsername='schafer5'
+    )
+
+response = request.execute()
+
+print(response)
